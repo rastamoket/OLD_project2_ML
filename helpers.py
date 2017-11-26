55ea615527e1c66ce10333a798867f5c43468ec2
rastamoket <stefan.froelicher@epfl.ch>
2017-11-26 15:42:18 +0100

    Start to take some functions from ex10 and adapting them

--- helpers.py
+++ helpers.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """some helper functions."""
 import numpy as np
+from itertools import groupby
+import scipy.sparse as sp # In order to use sparse 
 
 
-def load_data(dataSetCSVfile):
+def load_data_old(dataSetCSVfile): # This come from from the first project (modified)
     """Load data and convert it to the metrics system."""
     path_dataset = dataSetCSVfile # path of the data we want to load
     # To get the id of the samples
@@ -15,3 +17,86 @@ def load_data(dataSetCSVfile):
     data = np.genfromtxt(
         path_dataset, delimiter=",", skip_header=1, usecols=1)
     return rowCol_samples, data
+
+def create_csv_submission(ids, y_pred, name): # This come from the first project
+    """
+    Creates an output file in csv format for submission to kaggle
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, 'w') as csvfile:
+        fieldnames = ['Id', 'Prediction']
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({'Id':int(r1),'Prediction':int(r2)})
+
+
+
+def read_txt(path): # This come from the ex10, 'helpers.py'
+    """read text file from path."""
+    with open(path, "r") as f:
+        return f.read().splitlines()
+
+
+def load_data(path_dataset): # This come from the ex10, 'helpers.py'
+    """Load data in text format, one rating per line, as in the kaggle competition."""
+    data = read_txt(path_dataset)[1:]
+    return preprocess_data(data)
+
+
+def preprocess_data(data): # This come from the ex10, 'helpers.py'
+    """preprocessing the text data, conversion to numerical array format."""
+    def deal_line(line):
+        pos, rating = line.split(',')
+        row, col = pos.split("_")
+        row = row.replace("r", "")
+        col = col.replace("c", "")
+        return int(row), int(col), float(rating)
+
+    def statistics(data):
+        row = set([line[0] for line in data])
+        col = set([line[1] for line in data])
+        return min(row), max(row), min(col), max(col)
+
+    # parse each line
+    data = [deal_line(line) for line in data]
+
+    # do statistics on the dataset.
+    min_row, max_row, min_col, max_col = statistics(data)
+    print("number of users: {}, number of items: {}".format(max_row, max_col)) # Little modification here, in order to print the correct numbers
+
+    # build rating matrix.
+    ratings = sp.lil_matrix((max_row, max_col))
+    for row, col, rating in data:
+        ratings[row - 1, col - 1] = rating
+    return ratings
+
+
+def group_by(data, index): # This come from the ex10, 'helpers.py'
+    """group list of list by a specific index."""
+    sorted_data = sorted(data, key=lambda x: x[index])
+    groupby_data = groupby(sorted_data, lambda x: x[index])
+    return groupby_data
+
+
+def build_index_groups(train): # This come from the ex10, 'helpers.py'
+    """build groups for nnz rows and cols."""
+    nz_row, nz_col = train.nonzero()
+    nz_train = list(zip(nz_row, nz_col))
+
+    grouped_nz_train_byrow = group_by(nz_train, index=0)
+    nz_row_colindices = [(g, np.array([v[1] for v in value]))
+                         for g, value in grouped_nz_train_byrow]
+
+    grouped_nz_train_bycol = group_by(nz_train, index=1)
+    nz_col_rowindices = [(g, np.array([v[0] for v in value]))
+                         for g, value in grouped_nz_train_bycol]
+    return nz_train, nz_row_colindices, nz_col_rowindices
+
+
+def calculate_mse(real_label, prediction): # This come from the ex10, 'helpers.py'
+    """calculate MSE."""
+    t = real_label - prediction
+    return 1.0 * t.dot(t.T)
---
+++ pre_processing.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+''' Some functions to do some pre-processing '''
+import scipy.sparse as sp # In order to use sparse Matrix
+import numpy as np 
+
+def valid_ratings(ratings, num_items_per_user, num_users_per_item, min_num_ratings): # This is based on the ex10
+    ''' To select only the users and items that give "enough" ratings'''
+    valid_users = np.where(num_items_per_user >= min_num_ratings)[0]
+    valid_items = np.where(num_users_per_item >= min_num_ratings)[0]
+
+    valid_ratings = ratings[valid_items, :][: , valid_users]      
+    return valid_ratings
+
+def split_data(ratings, prob_test=0.1): # This is based on the ex10
+    ''' To split the data set in training and test '''
+    #******** Creation of the two matrices ***********
+    train = sp.lil_matrix((ratings.shape[0], ratings.shape[1])) # Training set 
+    test = sp.lil_matrix((ratings.shape[0], ratings.shape[1])) # Test set
+    
+    #******** Distribution of the samples in the two matrices ********
+    nonz_users, nonz_items = ratings.nonzero() # to have the indices of the elements that have non-zero rating
+    
+    for nz_u in set(nonz_users): # Loop over all the indices of the users that contain at least one non-zero value
+        r,c = ratings[nz_u, :].nonzero() # To have only the non-zero ratings for this user "nz_u"
+        te = np.random.choice(c, size=round(len(c)*prob_test)) # We choose randomly which values will go to the test (for each column)
+        tr = list(set(c) - set(te)) # All the others elements in this column are in the train
+        
+        test[nz_u,te] = ratings[nz_u,te] # Fill in the matrix test
+        train[nz_u, tr] = ratings[nz_u, tr] # Fill in the matrix train
+            
+    return train, test
+    
+    
